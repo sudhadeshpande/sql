0b5e0c1fb601bb1c8eb158b335232117d29da9a3
sudhadeshpande <54575914+sudhadeshpande@users.noreply.github.com>
2020-06-09 19:11:46 +0530

    Add files via upload

---
+++ FdDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineBanking.Models
+{
+	public class FdDetails
+	{
+		public List<FdDetails> FdDetailList { get; set; }
+		public long account_no { get; set; }
+		public int deposit_id  {get ;set;}
+	    public string fd_date { get; set; }
+        public decimal fd_amount { get; set; }
+	    public int duration { get; set; }
+	    public decimal rate_of_interest { get; set; }
+	    public string maturity_date { get; set; }
+	    public decimal maturity_amount { get; set; }
+	   public string nominee { get; set; }
+
+	}
+}
---
+++ FixedDepositClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineBanking.Models
+{
+    public class FixedDepositClass
+    {
+        [Required(ErrorMessage ="Please enter the Amount")]
+        [Display(Name ="Fixed Deposit Amount")]
+        public long fd_amount { get; set; }
+     
+        [Required(ErrorMessage = "Please enter the Amount")]
+        [Display(Name = "Fixed deposit Duration(in months)")]
+        public DateTime duration { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Nominee")]
+        [Display(Name = "Name of the Nominee")]
+        public string nominee { get; set; }
+
+    }
+}
